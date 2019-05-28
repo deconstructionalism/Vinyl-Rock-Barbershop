38ac266a0c656744ea1f57a905e3683909631370
Cody Gillette <cgillette1312@gmail.com>
2019-05-28 14:17:48 -0700

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+
+import LandingPage from './Pages/LandingPage';
+import ServicePage from './Pages/ServicePage';
+import LoginPage from './Pages/LoginPage';
+import BarberPage from './Pages/BarberPage';
+import Register from './Pages/Register';
+import AppointmentPage from './Pages/AppointmentPage';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <LandingPage/>
+        <ServicePage/>
+        <LoginPage/>
+        <BarberPage/>
+        <Register/>
+        <AppointmentPage/>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
