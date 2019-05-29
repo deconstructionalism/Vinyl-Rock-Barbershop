f4b0d83f579a9670377d03071309676edaf65eda
Cody Gillette <cgillette1312@gmail.com>
2019-05-29 13:29:01 -0700

    login working

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import LoginForm from '../Components/LoginForm/LoginForm'
 
 export default class LoginPage extends Component {
+    static defaultProps = {
+        location: {},
+        history: {
+            push: () => { },
+        },
+    }
+
+    handleLoginSuccess = () => {
+        const { location, history } = this.props
+        const destination = (location.state || {}).from || '/'
+        history.push(destination)
+    }
     render() {
         return (
             <div className='loginPage'>
-            <h2>Login</h2>
-            <LoginForm/>
+                <h2>Login</h2>
+                <LoginForm onLoginSuccess = {this.handleLoginSuccess}/>
             </div>
         )
     }
