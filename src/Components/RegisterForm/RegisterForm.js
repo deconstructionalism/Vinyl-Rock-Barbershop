c20e44c945d6276c156be6f09866c55b836b7f76
Cody Gillette <cgillette1312@gmail.com>
2019-05-30 08:32:29 -0700

    register

--- src/Components/RegisterForm/RegisterForm.js
+++ src/Components/RegisterForm/RegisterForm.js
@@ -1,61 +1,96 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import AuthApiService from '../../services/auth-api-service'
 
 export default class RegisterForm extends Component {
-    state= {error: null}
+    static defaultProps = {
+        onRegistrationSuccess: () => {}
+    }
+    state = { error: null }
 
-    handleNewUserSubmit = e =>{
+    handleNewUserSubmit = e => {
         e.preventDefault()
+        const { user_name,
+            first_name,
+            last_name,
+            email,
+            phone_number,
+            password } = e.target
+
+        this.setState({ error: null })
+        AuthApiService.postUser({
+            user_name: user_name.value,
+            first_name: first_name.value,
+            last_name: last_name.value,
+            email: email.value,
+            phone_number: phone_number.value,
+            password: password.value
+        })
+            .then(user => {
+                user_name.value = ''
+                first_name.value = ''
+                last_name.value = ''
+                email.value = ''
+                phone_number.value = ''
+                password.value = ''
+                this.props.onRegistrationSuccess()
+            })
+            .catch(res => {
+                this.setState({ error: res.error })
+            })
 
 
     }
     render() {
-        const {error} = this.state
+        const { error } = this.state
         return (
             <div>
                 <div>
                     <form
                         className='RegisterForm'
+                        onSubmit={this.handleNewUserSubmit}
                     >
                         <div role='alert'>
                             {error && <p className='red'>{error}</p>}
                         </div>
-                        <label className='First-Name'>
+                        <label className='First-Name-form'>
                             First Name
                         </label>
                         <input
                             type='text'
                             placeholder='First'
-                            name='First_name'
-                            
+                            name='first_name'
+                            required
+
                         />
-                        <label className='Last-Name'>
+                        <label className='Last-Name-form'>
                             Last Name
                         </label>
                         <input
                             type='text'
                             placeholder='Last'
-                            name='Last_name'
-                            
+                            name='last_name'
+                            required
+
                         />
-                        <label className='userName'>
+                        <label className='userName-form'>
                             User Name
                         </label>
                         <input
                             type='text'
                             placeholder='First_Last...'
                             name='user_name'
-                            
+                            required
                         />
-                        <label className='Password'>
+                        <label className='Password-form'>
                             Password
                         </label>
                         <input
                             type='password'
                             name='password'
-                            
+                            required
                         />
-                        <label className='phone_number'>
+                        <label className='phone_number-form'>
                             Phone Number
                         </label>
                         <input
@@ -63,21 +98,23 @@ export default class RegisterForm extends Component {
                             pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                             placeholder='XXX-XXX-XXXX'
                             name='phone_number'
+                            required
                         />
-                        <label className='email'>
+                        <label className='email-form'>
                             Email
                         </label>
                         <input
                             type='email'
                             placeholder='First@gmail.com'
                             name='email'
+                            required
                         />
                         <button type='submit'>
                             Register
                         </button>
                     </form>
                 </div>
-                
+
             </div>
         )
     }
--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import RegisterForm from '../Components/RegisterForm/RegisterForm'
 
 export default class Register extends Component {
+    static defaultProps = {
+        history: {
+            push: () => { },
+        },
+    }
+
+    handleRegistrationSuccess = user => {
+        const { history } = this.props
+        history.push('/login')
+    }
     render() {
         return (
             <div>
                 <h3> Register </h3>
-                <RegisterForm/>
+                <RegisterForm
+                  onRegistrationSuccess = {this.handleRegistrationSuccess}
+                />
             </div>
         )
     }
