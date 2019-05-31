d558d8fba810a3a0f69bdfc5aca12c48c966dca8
Cody Gillette <cgillette1312@gmail.com>
2019-05-31 15:18:03 -0700

    barber

--- src/Components/BarberList/BarberList.js
+++ src/Components/BarberList/BarberList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Barber from '../Barber/Barber'
+import barberApiService from '../../services/barber-api-service'
 
 export default class BarberList extends Component {
     render() {
