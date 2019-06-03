e995c73f220170b1fd536097bbf2c2212e237e4d
Cody Gillette <cgillette1312@gmail.com>
2019-06-03 12:15:41 -0700

    service buttons front to back

--- src/Components/BarberProfile/BarberProfile.js
+++ src/Components/BarberProfile/BarberProfile.js
@@ -1,77 +1,32 @@
 import React, { Component } from 'react'
 import BarberApiService from '../../services/barber-api-service'
-import { TimeButtons } from '../Utilitys/Utils'
+import { TimeButtons, ServiceButtons } from '../Utilitys/Utils'
 export default class BarberProfile extends Component {
     state = {
         barberInfo: [],
-         hasError: false 
+        hasError: false
     }
     static defaultProps = {
-        match: {params: {}}
+        match: { params: {} }
     }
-    componentDidMount(){
-        const {barberid} = this.props.match.params
+    componentDidMount() {
+        const { barberid } = this.props.match.params
         BarberApiService.getBarber(barberid)
             .then(data => {
-                return this.setState({barberInfo:data})
+                return this.setState({ barberInfo: data })
             })
-    }
-    renderTitle(){
-        return this.barberInfo.map(barber => 
-            console.log(barber))
-
-
-    }
+        }
     render() {
-        const {first_name } = this.state.barberInfo
+        const { first_name } = this.state.barberInfo
         return (
             <div>
                 <h1>{first_name}</h1>
                 <h3>Choose your Service</h3>
                 <form className='service-list'>
-                    <button value='hairCut'>
-                        <h3>HAIRCUT</h3>
-                        <p>$20 45min</p>
-                    </button>
-                    <button value='buzzCut'>
-                        <h3>BUZZCUT</h3>
-                        <p>$15 45min</p>
-                    </button>
-                    <button value='kids'>
-                        <h3>KID 12 AND UNDER </h3>
-                        <p>$10 45min</p>
-                    </button>
-                    <button value='seniors'>
-                        <h3>SENIOR CUT 60+</h3>
-                        <p>$10 60min</p>
-                    </button>
-                    <button value='designs'>
-                        <h3>DESIGNS</h3>
-                        <p>$5</p>
-                    </button>
-                    <button value='hairwash'>
-                        <h3>HAIRWASH</h3>
-                        <p>$5</p>
-                    </button>
-                    <button value='beardTrim'>
-                        <h3>BEARD TRIM</h3>
-                        <p>$10 15min</p>
-                    </button>
-                    <button value='beardTrimRazorFinish'>
-                        <h3>BEARD TRIM (RAZOR FINISH)</h3>
-                        <p>$15 25min</p>
-                    </button>
-                    <button value='beardTrim'>
-                        <h3> HAIRCUT AND BEARD TRIM </h3>
-                        <p>$30 60min</p>
-                    </button>
-                    <button value='luxuryShave'>
-                        <h3>LUXURY SHAVE</h3>
-                        <p>$30 45min</p>
-                    </button>
+                <ServiceButtons/>
                 </form>
                 <h3>Pick Time</h3>
-                <TimeButtons/>
+                <TimeButtons />
                 <button type='submit'>Review/Book</button>
             </div>
         )
--- src/Components/Utilitys/Utils.js
+++ src/Components/Utilitys/Utils.js
@@ -1,46 +1,82 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export function TimeButtons() {
-    return (
-        <div>
-            <form>
-                <button>
-                    <p>7:00 a.m</p>
+import BarberApiService from '../../services/barber-api-service';
+
+export class ServiceButtons extends Component {
+    state = {
+        barberServices: []
+    }
+    componentDidMount() {
+        BarberApiService.getBarberServices()
+            .then(services => {
+                console.log(services)
+                return this.setState({ barberServices: services })
+
+            })
+        }
+
+
+        renderServices() {
+            return this.state.barberServices.map(barber => {
+                return <button key={barber.id} >
+                    <h3>{barber.type}</h3>
+                    <p>{barber.cost} 45min</p>
                 </button>
-                <button>
-                    <p>8:00 a.m</p>
-                </button>
-                <button>
-                    <p>9:00 a.m</p>
-                </button>
-                <button>
-                    <p>10:00 a.m</p>
-                </button>
-                <button>
-                    <p>11:00 a.m</p>
-                </button>
-                <button>
-                    <p>1:00 p.m</p>
-                </button>
-                <button>
-                    <p>2:00 p.m</p>
-                </button>
-                <button>
-                    <p>3:00 p.m</p>
-                </button>
-                <button>
-                    <p>4:00 p.m</p>
-                </button>
-                <button>
-                    <p>5:00 p.m</p>
-                </button>
-                <button>
-                    <p>6:00 p.m</p>
-                </button>
-                <button>
-                    <p>7:00 p.m</p>
-                </button>
-            </form>   
-        </div>
-    )
+            })
+        }
+        render() {
+            return (
+                <div>
+                    {this.renderServices()}
+                </div>
+            )
+        }
+    }
+
+
+export class TimeButtons extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <button>
+                        <p>7:00 a.m</p>
+                    </button>
+                    <button>
+                        <p>8:00 a.m</p>
+                    </button>
+                    <button>
+                        <p>9:00 a.m</p>
+                    </button>
+                    <button>
+                        <p>10:00 a.m</p>
+                    </button>
+                    <button>
+                        <p>11:00 a.m</p>
+                    </button>
+                    <button>
+                        <p>1:00 p.m</p>
+                    </button>
+                    <button>
+                        <p>2:00 p.m</p>
+                    </button>
+                    <button>
+                        <p>3:00 p.m</p>
+                    </button>
+                    <button>
+                        <p>4:00 p.m</p>
+                    </button>
+                    <button>
+                        <p>5:00 p.m</p>
+                    </button>
+                    <button>
+                        <p>6:00 p.m</p>
+                    </button>
+                    <button>
+                        <p>7:00 p.m</p>
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
